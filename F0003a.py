777ebe7bea33c89ce101d6f05348ed25bab829eb
szikorabence <szikora.bence@wm-iskola.hu>
2020-11-05 08:40:29 +0000

    Initial commit

---
+++ F0003a.py
@@ -0,0 +1,3 @@
+név = input('Mi a kereszt neved?')
+név2 = input('Mi a Vezeték neved?')
+print('Szia örülök ', név,név2,  ',hogy megismertelek.',  sep='')
---
+++ F0003b.py
@@ -0,0 +1,4 @@
+születés = input('Mikor születtél?')
+csillagjegy = input('Mi a csillagjegyed?')
+print('Te ', születés, ',születtél.',  sep='')
+print('A te csillagjegyed ', csillagjegy, ',.',  sep='')
